98f549381673fd6500d69ff592efda832d93cb8b
tivvon99 <72590560+tivvon99@users.noreply.github.com>
2023-08-06 15:26:31 -0400

    Rerranged

---
+++ .venv/Volunteer_Now/src/app.py
@@ -0,0 +1,61 @@
+'''
+The main application file for server
+'''
+
+from common.database import Database
+from models.user import User
+
+
+from flask import Flask, render_template, request, session, make_response, url_for, redirect
+
+
+app = Flask(__name__, static_url_path='/static')  # '__main__'
+app.secret_key = "Nuggets"
+
+
+@app.route('/')
+def home_template():
+    return render_template('home.html')
+
+
+        
+@app.route('/login')
+def login_template():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register_template():
+    return render_template('register.html')
+
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login_valid(email, password):
+        User.login(email)
+    else: # update to where we say user is not valid try again
+        session['email'] = None
+
+    return render_template("profile.html", email=session['email'])
+
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
+    
+    return render_template('profile.html', email=session['email'])
+
+
+if __name__ == '__main__':
+    app.run(port=4995, debug=True)
--- main.py
+++
@@ -1,3 +0,0 @@
-from Volunteer_Now.src.app import app
-if __name__ == "__main__":
-    app.run(debug=True)
